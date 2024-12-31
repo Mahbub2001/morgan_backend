814e131d457de5abb5ba9be3e5e5710b3a640b8a
Mahbub2001 <imahbubturza@gmail.com>
2024-12-31 13:49:39 +0600

    cors

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const app = express();
 // const cloudinary = require("cloudinary").v2;
 const port = process.env.PORT || 5000;
 // const fileUpload = require("express-fileupload");
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000", 
+  methods: ["GET", "POST", "PUT", "DELETE","PATCH"], 
+  allowedHeaders: ["Content-Type", "Authorization"], 
+};
+app.use(cors(corsOptions));
+
+// app.use(cors());
 app.use(express.json());
 
 // app.use(
