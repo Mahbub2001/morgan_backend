424d1fccefb547c59215b24850466c9e6771c54b
Mahbub2001 <imahbubturza@gmail.com>
2024-12-31 14:17:09 +0600

    cors

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const port = process.env.PORT || 5000;
 // const fileUpload = require("express-fileupload");
 app.use(cors({
   origin: ["http://localhost:3000"],
-  credentials: true, 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true, // Allows cookies and authorization headers
 }));
-
 // app.use(cors());
 app.use(express.json());
 
