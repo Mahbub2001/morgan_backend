df27da2648356b8abc3f1715b480d81b191becb5
Mahbub2001 <imahbubturza@gmail.com>
2024-12-31 15:49:05 +0600

    vercel

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200,
   })
 );
-
 // app.use(cors());
 app.use(express.json());
 
